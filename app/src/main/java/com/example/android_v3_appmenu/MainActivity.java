a9b0226fc1584d365ac323a4cdc93ac5811dd394
mossoii84 <mossomkh@gmail.com>
2020-08-02 22:14:56 +0200

    menu and menuLand

---
+++ app/src/main/java/com/example/android_v3_appmenu/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android_v3_appmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static com.example.android_v3_appmenu.R.id.itemPlus;
+public class MainActivity extends AppCompatActivity {
+
+    private boolean x = false;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override   // ставим верхнее меню
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu); //указываем menu андройд сам выберит какое выводить на экран обысное/land
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //При нажатии появляется/проподает выпадающее боковая панель
+    @Override  // по нажатию на кнопку плюс в меню, появляется всплывающее окно
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        TextView textView = (TextView)findViewById(R.id.textForPlus);
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.linerForVerticalMenu);
+        int id = item.getItemId();
+        // itemPlus -  добавил над классом import static com.example.android_v3_appmenu.R.id.itemPlus;
+        if (id == itemPlus && !x) {
+            linearLayout.setVisibility(View.VISIBLE);
+            textView.setText("есть меню");
+            x=true;
+        }
+        else if (x) {
+            linearLayout.setVisibility(View.INVISIBLE);
+            textView.setText("нет меню");
+            x=false;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
